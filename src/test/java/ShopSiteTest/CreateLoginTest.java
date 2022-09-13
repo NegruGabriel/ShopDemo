f16b4afd293456d38573d8c6cb4268823c1bd4ce
NegruGabriel <87751537+NegruGabriel@users.noreply.github.com>
2022-09-13 12:51:05 +0200

    Teste Logare

--- src/test/java/ShopSiteTest/CreateLoginTest.java
+++ src/test/java/ShopSiteTest/CreateLoginTest.java
@@ -5,10 +5,10 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
-public class LoginPageTest extends SharedData {
+public class CreateLoginTest extends SharedData {
 
     @Test
-    public  void  LoginPageMethod(){
+    public  void  CreateLoginMethod(){
 
         WebElement DismissLink = driver.findElement(By.className("woocommerce-store-notice__dismiss-link"));
         DismissLink.click();
@@ -25,11 +25,23 @@ public class LoginPageTest extends SharedData {
         EmailAddressElement .sendKeys( EmailAddressValue);
 
         WebElement PasswordElement = driver.findElement(By.cssSelector("input[id='reg_password']"));
-        String PasswordValue ="parolamare";
+        String PasswordValue ="parolamarePAROLAMARE@123456";
         PasswordElement .sendKeys(PasswordValue);
 
-       // WebElement RegisterButton= driver.findElement(By.cssSelector([name='register']);
-       // RegisterButton.click();
+        WebElement RegisterButton= driver.findElement(By.cssSelector("button[name='register']"));
+       RegisterButton.click();
+
+        driver.navigate().to("https://shop.demoqa.com/shop-demoqa?redirect_to=https%3A%2F%2Fshop.demoqa.com%3A443%2Fmy-account%2F&aiowps_login_msg_id=session_expired\n");
+
+       WebElement EmailAddressAccountElement = driver.findElement(By.id("user_login"));
+        String  EmailAddressAccountValue ="vash@yahoo.co";
+        EmailAddressAccountElement.sendKeys( EmailAddressAccountValue);
+
+        WebElement PasswordAccountElement = driver.findElement(By.id("user_pass"));
+        String PasswordAccountValue ="parolamarePAROLAMARE@123456";
+        PasswordAccountElement.sendKeys(PasswordAccountValue);
+
+
 
   }
 
